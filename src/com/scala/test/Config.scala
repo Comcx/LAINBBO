a470950029d2d9f966a526b1582bf1682dd7b17d
Comcx <comcx@outlook.com>
2017-08-28 18:16:23 +0800

    Update Config.scala

--- src/com/scala/test/Config.scala
+++ src/com/scala/test/Config.scala
@@ -4,11 +4,12 @@ class Config(var problem: Problem) {
   
   var populationSize: Int = 100
   var eliteSize: Int = 10
-  var chromLength: Int = 10
-  var chromPerElemRange = Array(0,1)
+  var chromLength: Int = 6
+  var neuronSize = 4
+  var chromPerElemRange: Array[Double] = Array(0,1)
   var generation = 100
   var probability_modify = 0.75
   var probability_mutate = 0.05
-  var F = 0.5
+  var K = Array( 0.02, 0.4, 0.95, 0.1, 0.25 )
   
 }
